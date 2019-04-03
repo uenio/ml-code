c707df8fa16934fd72a4da35b7024f471c0c68c4
uenio <han.zhao@stonybrook.edu>
2019-04-03 18:53:07 -0400

    Reading csv file

---
+++ Perceptrons/Perceptron.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+class Perceptron(object):
+    def __init__ (self):
+        f = open("Breast_cancer_data.csv")
+        print (f.read())
+        
+perceptron = Perceptron()
