cc0d41fa9d980647d0db580d10134291cd69964a
uenio <han.zhao@stonybrook.edu>
2019-04-03 20:35:47 -0400

    finished file reading

--- Perceptrons/Perceptron.py
+++ Perceptrons/Perceptron.py
@@ -2,8 +2,30 @@ import numpy as np
 
 
 class Perceptron(object):
+    vectors = []
+    weights = []
+    
+    def stringListToFloatList(self, strList):
+        floatList = []
+        for i in strList:
+            floatList.append(float(i))
+        return floatList
+    
     def __init__ (self):
-        f = open("Breast_cancer_data.csv")
-        print (f.read())
-        
+        # open file and skip first line
+        f = open("Breast_cancer_data.csv", "r")
+        next(f)
+
+        # read line by line
+        for line in f:
+            # split string by commas and transform into float lists
+            curStrVec = line.split(',')
+            curFloatVec = self.stringListToFloatList(curStrVec)
+            # add to list of vectors
+            self.vectors.append(curFloatVec)
+
+        # instantiate weights to 0 and set weight[0] to bias
+        self.weights = np.zeros(len(curFloatVec))
+
+    
 perceptron = Perceptron()
